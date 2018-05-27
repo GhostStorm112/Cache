e2653da2c2340872e795fc8094856e4e0bb43a86
112madgamer <ryleegeorge1@gmail.com>
2018-05-27 19:09:22 +1000

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+require('bluebird')
+const Core = require('Core')
+// const config = require('../config.json')
+const RainCache = require('raincache')
+
+const log = new Core.Logger()
+
+const amqp = new RainCache.Connectors.AmqpConnector({
+  amqpUrl: process.env.AMQP_URL || 'amqp://localhost',
+  amqpQueue: 'weather-pre-cache',
+  sendQueue: 'weather-events'
+})
+const redis = new RainCache.Engines.RedisStorageEngine({ password: process.env.REDIS_PASS })
+
+const cache = new RainCache({ storage: { default: redis }, debug: false }, amqp, amqp)
+
+async function run () {
+  log.info('STARTUP', 'Starting Cache')
+  await cache.initialize()
+  cache.on('error', error => log.error('GWError', error))
+  log.info('STARTUP', 'Cache worker running')
+}
+
+run().catch(error => log.error('STARTUP\n', error))
