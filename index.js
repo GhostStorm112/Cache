dc13e8d0a53f5a530c97da928ac171ab33ea126a
112madgamer <ryleegeorge1@gmail.com>
2018-05-30 23:37:06 +1000

    Redis change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const amqp = new RainCache.Connectors.AmqpConnector({
   amqpQueue: 'weather-pre-cache',
   sendQueue: 'weather-events'
 })
-const redis = new RainCache.Engines.RedisStorageEngine({ host: process.env.REDIS_URL || 'redis_db', password: process.env.REDIS_PASS })
+const redis = new RainCache.Engines.RedisStorageEngine({ host: process.env.REDIS_URL, password: process.env.REDIS_PASS })
 
 const cache = new RainCache({ storage: { default: redis }, debug: false }, amqp, amqp)
 
