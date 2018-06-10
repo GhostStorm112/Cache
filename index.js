a3e066fb52a834b09a248bf4a581b7c5c7b1b218
112madgamer <ryleegeorge1@gmail.com>
2018-06-10 16:02:44 +1000

    Added stats

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ require('dotenv').config()
 
 const Core = require('Core')
 const RainCache = require('raincache')
-
+const AmqpConnector = require('./DetailedAmqpConnector')
 const log = new Core.Logger()
+let StatsD
+let statsClient
+try {
+  StatsD = require('hot-shots')
+} catch (e) {
 
-const amqp = new RainCache.Connectors.AmqpConnector({
+}
+if (process.env.STATSD) {
+  statsClient = new StatsD({
+    host: process.env.STATSD_HOST,
+    port: process.env.STATSD_PORT,
+    prefix: process.env.STATSD_PREFIX,
+    telegraf: true
+  })
+}
+const amqp = new AmqpConnector({
   amqpUrl: process.env.AMQP_URL || 'amqp://localhost',
   amqpQueue: 'weather-pre-cache',
   sendQueue: 'weather-events'
-})
+}, statsClient)
 const redis = new RainCache.Engines.RedisStorageEngine({ host: process.env.REDIS_URL, password: process.env.REDIS_PASS })
 
 const cache = new RainCache({ storage: { default: redis }, debug: false }, amqp, amqp)
